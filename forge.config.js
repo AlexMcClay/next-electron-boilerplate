b1b50d42bbc668bf20b29f89cd25ab3d85b7a301
AlexMcClay <smurf.alix@gmail.com>
2023-05-25 02:53:22 +0930

    made ekectron run next.js 13 as static export

--- forge.config.js
+++ forge.config.js
@@ -1,21 +1,26 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    ignore: [
+      "^\\/src$",
+      // [...]
+    ],
+  },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-squirrel",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
     },
     {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
   ],
---
+++ main/main.js
@@ -0,0 +1,59 @@
+// Main File for Electron
+
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const serve = require("electron-serve");
+
+function handleSetTitle(event, title) {
+  const webContents = event.sender;
+  const win = BrowserWindow.fromWebContents(webContents);
+  win.setTitle(title);
+}
+
+// run renderer
+const isProd = process.env.NODE_ENV !== "development";
+if (isProd) {
+  serve({ directory: "build" });
+} else {
+  app.setPath("userData", `${app.getPath("userData")} (development)`);
+}
+
+console.log(process.env.NODE_ENV, isProd);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      enableRemoteModule: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  ipcMain.on("set-title", (event, title) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    win.setTitle(title);
+  });
+
+  if (isProd) {
+    win.loadURL("app://./home.html");
+  } else {
+    const port = process.argv[2];
+    win.loadURL("http://localhost:3000/");
+  }
+};
+
+app.whenReady().then(() => {
+  ipcMain.on("set-title", handleSetTitle);
+
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
