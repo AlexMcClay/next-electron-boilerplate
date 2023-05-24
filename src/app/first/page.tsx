93198d677e03bff6dc6fd7310afa679c43ddb092
AlexMcClay <smurf.alix@gmail.com>
2023-05-25 04:08:10 +0930

    added some test pages

---
+++ src/app/first/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <b>First</b>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/second/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <b>Seconds</b>
+    </div>
+  );
+};
+
+export default page;
